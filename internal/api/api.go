09605f2e3da2086035c13311ff1756aded813c5e
jordicido <jordi.cidoncha4@gmail.com>
2023-11-02 10:54:02 +0100

    Explore pokemons in area

--- internal/api/api.go
+++ internal/api/api.go
@@ -11,11 +11,15 @@ import (
 	Cache "github.com/jordicido/pokedexcli/internal/cache"
 )
 
+type LocationArea struct {
+	Name string
+	Url  string
+}
+
 var cache *Cache.Cache
 
-func CallPokeLocationArea(link *string) ([]string, *string, *string) {
-
-	var locations []string
+func GetLocations(link *string) ([]LocationArea, *string, *string) {
+	var locations []LocationArea
 	type response struct {
 		Count    int     `json:"count"`
 		Next     *string `json:"next"`
@@ -74,7 +78,94 @@ func CallPokeLocationArea(link *string) ([]string, *string, *string) {
 		fmt.Println(err)
 	}
 	for _, location := range apiResponse.Results {
-		locations = append(locations, location.Name)
+		locations = append(locations, LocationArea{Name: location.Name, Url: location.URL})
 	}
 	return locations, apiResponse.Next, apiResponse.Previous
+}
+
+func GetPokemonsInArea(area string) []string {
+	var pokemons []string
+	url := "https://pokeapi.co/api/v2/location-area/"
+	type response struct {
+		EncounterMethodRates []struct {
+			EncounterMethod struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"encounter_method"`
+			VersionDetails []struct {
+				Rate    int `json:"rate"`
+				Version struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"version"`
+			} `json:"version_details"`
+		} `json:"encounter_method_rates"`
+		GameIndex int `json:"game_index"`
+		ID        int `json:"id"`
+		Location  struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"location"`
+		Name  string `json:"name"`
+		Names []struct {
+			Language struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"language"`
+			Name string `json:"name"`
+		} `json:"names"`
+		PokemonEncounters []struct {
+			Pokemon struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"pokemon"`
+			VersionDetails []struct {
+				EncounterDetails []struct {
+					Chance          int   `json:"chance"`
+					ConditionValues []any `json:"condition_values"`
+					MaxLevel        int   `json:"max_level"`
+					Method          struct {
+						Name string `json:"name"`
+						URL  string `json:"url"`
+					} `json:"method"`
+					MinLevel int `json:"min_level"`
+				} `json:"encounter_details"`
+				MaxChance int `json:"max_chance"`
+				Version   struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"version"`
+			} `json:"version_details"`
+		} `json:"pokemon_encounters"`
+	}
+	var body []byte
+	var err error
+	cachedData, ok := cache.Get(url + area)
+	if ok {
+		body = cachedData
+	} else {
+		res, err := http.Get(url + area)
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err = io.ReadAll(res.Body)
+		res.Body.Close()
+		if res.StatusCode > 299 {
+			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		cache.Add(url+area, body)
+	}
+
+	apiResponse := &response{}
+	err = json.Unmarshal(body, apiResponse)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, pokemon := range apiResponse.PokemonEncounters {
+		pokemons = append(pokemons, pokemon.Pokemon.Name)
+	}
+	return pokemons
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	Api "github.com/jordicido/pokedexcli/internal/api"
 )
@@ -11,7 +12,7 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(interface{}) error
 }
 
 var config struct {
@@ -21,7 +22,7 @@ var config struct {
 
 var commands map[string]cliCommand
 
-func commandHelp() error {
+func commandHelp(interface{}) error {
 	fmt.Printf("\nWelcome to the Pokedex!\nUsage:\n\n")
 	for name := range commands {
 		fmt.Printf("%s: %s \n", commands[name].name, commands[name].description)
@@ -29,33 +30,39 @@ func commandHelp() error {
 	return nil
 }
 
-func commandExit() error {
+func commandExit(interface{}) error {
 	os.Exit(0)
 	return nil
 }
 
-func commandMap() error {
-	var locations []string
-	locations, config.Next, config.Previous = Api.CallPokeLocationArea(config.Next)
+func commandMap(interface{}) error {
+	var locations []Api.LocationArea
+	locations, config.Next, config.Previous = Api.GetLocations(config.Next)
 	for _, location := range locations {
-		fmt.Println(location)
+		fmt.Println(location.Name)
 	}
 
 	return nil
 }
 
-func commandMapB() error {
-	var locations []string
-	locations, config.Next, config.Previous = Api.CallPokeLocationArea(config.Previous)
+func commandMapB(interface{}) error {
+	var locations []Api.LocationArea
+	locations, config.Next, config.Previous = Api.GetLocations(config.Previous)
 	for _, location := range locations {
-		fmt.Println(location)
+		fmt.Println(location.Name)
 	}
 
 	return nil
 }
 
-func commandExplore() error {
-	//TODO
+func commandExplore(area interface{}) error {
+	fmt.Printf("Exploring %s...\n", area)
+	fmt.Println("Found Pokemon:")
+
+	pokemons := Api.GetPokemonsInArea(area.(string))
+	for _, pokemon := range pokemons {
+		fmt.Printf("- %s\n", pokemon)
+	}
 
 	return nil
 }
@@ -97,10 +104,14 @@ func main() {
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		input := scanner.Text()
-		command, ok := commands[input]
+		input := strings.Split(scanner.Text(), " ")
+		command, ok := commands[input[0]]
 		if ok {
-			command.callback()
+			var param interface{}
+			if len(input) > 1 {
+				param = input[1]
+			}
+			command.callback(param)
 		} else {
 			fmt.Println("This command is not valid")
 		}
